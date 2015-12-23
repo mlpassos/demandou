bc85bf544ddf4bf2b777414fe67f708edcb74c48
Márcio Passos <ask@me.com>
2015-12-23 17:05:55 -0200

    Update
    Updated to new template views

---
+++ application/controllers/Auth_controller.php
@@ -0,0 +1,70 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth_controller extends MY_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+
+	public function index()	{
+		
+	}
+
+	public function autenticar() {
+		$usuario = $this->input->post('usuario');
+		$senha = $this->input->post('senha');
+		// autenticar
+		$this->load->model('usuario_model');
+		if ($data['usuario'] = $this->usuario_model->listarPorUsuarioSenha($usuario,$senha)) {
+				$res = array();
+				$resultado = array();
+				foreach($data['usuario'] as $usuario) :
+					$resultado['login'] = $usuario['login'];
+					$resultado['nome'] = $usuario['nome'];
+					$resultado['sobrenome'] = $usuario['sobrenome'];
+					$resultado['codigo_perfil'] = $usuario['codigo_perfil'];
+					$resultado['codigo_status'] = $usuario['codigo_status'];
+					array_push($res, $resultado);
+				endforeach;
+				// loga, cria session
+				$this->session->set_userdata( array(
+		            'login'=> $resultado['login'],
+		            'nome'=> $resultado['nome'],
+		            'sobrenome'=>$resultado['sobrenome'],
+		            'codigo_perfil'=>$resultado['codigo_perfil'],
+		            'codigo_status'=>$resultado['codigo_status'],
+		            'logado' => true
+		        ));
+		        echo json_encode($resultado);
+		        // echo json_encode('ok');
+		} else {
+			$this->session->set_userdata( array('logado' => false) );
+			echo json_encode(array("status"=>"falha"));
+		}
+       // var_dump($resultado);
+        // echo 'logado ' .  $this->session->userdata('usuario');
+        //echo json_encode($this->session->userdata());
+        //redirect('/welcome');
+	}
+
+	public function logout() {
+		$usuario = $this->input->post('usuario');
+		// destroi session
+		$this->session->sess_destroy();
+		echo "saiu";
+	
+	}
+}
--- application/controllers/Usuario_controller.php
+++ application/controllers/Usuario_controller.php
@@ -21,6 +21,11 @@ class Usuario_controller extends MY_Controller {
 
 	public function __construct() {
         parent::__construct();
+
+        if ((int) $this->session->userdata('codigo_perfil')!==2) {
+        	// echo getType($this->session->userdata('codigo_perfil'));
+        	redirect(base_url());
+        }
     }
 
 	public function index()	{
@@ -43,23 +48,33 @@ class Usuario_controller extends MY_Controller {
 			"content" => "admin,demandou,demandas, html5, sistema"
 			)
 		);
+		// MENU
+		$data_header['menu']=array(
+			array(
+				"name" => "Projetos",
+				"link" => base_url() . 'projetos'
+				),
+				array(
+				"name" => "Usuários",
+				"link" => base_url() . 'usuarios'
+			)
+		);
 		// CSS
 		$data_header['css']=array(array('file' => 'estilos-principal.css')); 
 		// JS
 		$data_footer['js']=array(
 			array('file' => 'http://cdnjs.cloudflare.com/ajax/libs/masonry/3.3.2/masonry.pkgd.min.js'), 
-			array('file' =>  base_url() . '/assets/js/admin.js'),
-			array('file' =>  base_url() . '/assets/js/usuarios.js')
+			array('file' =>  base_url() . 'assets/js/admin.js'),
+			array('file' =>  base_url() . 'assets/js/usuarios.js')
 		);
 
-		$this->load->view('admin/usuarios/header_view',$data_header);
+		$this->load->view('header_view',$data_header);
 		$this->load->view('admin/usuarios/content_view',$conteudo);
-		$this->load->view('admin/usuarios/footer_view',$data_footer);		
+		$this->load->view('footer_view',$data_footer);		
 	}
 
 
-	function do_upload()
-	{
+	function do_upload() {
 		$config['upload_path'] = './uploads/';
 		$config['allowed_types'] = 'gif|jpg|png';
 		$config['max_size']	= '0';
@@ -83,40 +98,66 @@ class Usuario_controller extends MY_Controller {
 	}
 
 	public function adicionar() {
-		$data['css']= "estilos-principal.css";
+		// META
+		$data_header['meta']=array(
+			array(
+			"name" => "title",
+			"content" => "Demandou - Adicionar Usuário"
+			),
+			array(
+			"name" => "description",
+			"content" => "Adicionar usuário..."
+			),
+			array(
+			"name" => "keywords",
+			"content" => "admin,demandou,demandas, html5, sistema"
+			)
+		);
+		// MENU
+		$data_header['menu']=array(
+			array(
+				"name" => "Projetos",
+				"link" => base_url() . 'projetos'
+				),
+				array(
+				"name" => "Usuários",
+				"link" => base_url() . 'usuarios'
+			)
+		);
+		// CSS
+		$data_header['css']=array(array('file' => 'estilos-principal.css')); 
+		// JS
+		$data_footer['js']=array(
+			array('file' =>  base_url() . 'assets/js/admin.js'),
+			array('file' =>  base_url() . 'assets/js/usuarios_adicionar.js')
+		);
 		
-		if( $this->session->userdata('logado') ) {
-        	$data['usuario'] = $this->session->userdata();
+		// load data for user form
+		// if( $this->session->userdata('logado') ) {
+			$this->load->helper('form');
+			$this->load->library('form_validation');
+			
         	$this->load->model('funcao_model');
         	$this->load->model('perfil_model');
         	$auxiliar['funcao'] = $this->funcao_model->listar();
         	$auxiliar['perfil'] = $this->perfil_model->listar();
-        	// pegar demandas de acordo com código do usuário
-    	} else {
-	         $data['usuario'] = array('logado'=>false);
-	    }
+    	// }
+    	
+		$this->load->view('header_view',$data_header);
 		
-		$this->load->view('admin/usuarios/header_view',$data);
-		
-		if (isset($auxiliar['funcao'])) {
-			$this->load->helper('form');
-			$this->load->library('form_validation');
-			if ($this->form_validation->run() == FALSE)
-			{
+		// if( $this->session->userdata('logado') ) {
+			if ($this->form_validation->run() == FALSE) {
 				// se não é válido o formulário, recarrega página com mensagens de erro.
 				$this->load->view('admin/usuarios/adicionar_view', $auxiliar);
 			}
-			else
-			{
+			else {
 				$usuario = $this->input->post();
-				// $foto = $this->do_upload();
-				// echo "<pre>a";
-				//  var_dump($foto);
-				// echo "</pre>";
+
 				$up = $this->do_upload();
 				$data['upload_data'] = $this->upload->data();
 				$usuario['arquivo_avatar'] = $this->upload->data('file_name');
 
+				// scale image to thumbnail size 120x120
 				$config['image_library'] = 'gd2';
 				$config['source_image']	= './uploads/' . $this->upload->data('file_name');
 				$config['create_thumb'] = FALSE;
@@ -125,29 +166,24 @@ class Usuario_controller extends MY_Controller {
 				$config['height']	= 120;
 
 				$this->load->library('image_lib', $config); 
-
 				$this->image_lib->resize();
-				// echo "<pre>";
-				//   var_dump($usuario);
-				// echo "</pre>";
 
+				// add user to database
 				$this->load->model('usuario_model');
-				// $this->usuario_model->inserir($usuario);
 				if ($this->usuario_model->inserir($usuario)) {
 					$data['nome'] = $usuario['nome'];
 					$this->load->view('admin/usuarios/adicionar_sucesso_view.php', $data);
 				} else {
 					echo "Oops, deu bug. Tente novamente? =]";
 				}
-				// sucesso, cria usuário
-				//$this->load->view('formsuccess');
 			}
-		} else {
-			$this->load->view('admin/usuarios/adicionar_view');	
-		}
-		$this->load->view('admin/usuarios/footer_view');	
+		// } else {
+			// não está logado
+		// 	$this->load->view('admin/usuarios/adicionar_view');	
+		// }
+		$this->load->view('footer_view', $data_footer);	
 	}
-	public function checar_login($login) {
+	function checar_login($login) {
 		//$usuario = "mpassos";
 		$this->load->model('usuario_model');
 		$realuser = $this->usuario_model->listarPorUsuario($login);
@@ -162,7 +198,7 @@ class Usuario_controller extends MY_Controller {
 			return TRUE;
 		}
 	}
-	public function check_default($element) {
+	function check_default($element) {
 		// foreach($array /as $element) {
 		// echo $element;
 	    	if($element == 'escolha') {   
@@ -171,50 +207,47 @@ class Usuario_controller extends MY_Controller {
 	  // 	}
 	 	// return TRUE;
 	}
-	public function autenticar() {
-		$usuario = $this->input->post('usuario');
-		$senha = $this->input->post('senha');
-		// autenticar
-		$this->load->model('usuario_model');
-		if ($data['usuario'] = $this->usuario_model->listarPorUsuarioSenha($usuario,$senha)) {
-				$res = array();
-				$resultado = array();
-				foreach($data['usuario'] as $usuario) :
-					$resultado['login'] = $usuario['login'];
-					$resultado['nome'] = $usuario['nome'];
-					$resultado['sobrenome'] = $usuario['sobrenome'];
-					$resultado['codigo_perfil'] = $usuario['codigo_perfil'];
-					$resultado['codigo_status'] = $usuario['codigo_status'];
-					array_push($res, $resultado);
-				endforeach;
-				// loga, cria session
-				$this->session->set_userdata( array(
-		            'login'=> $resultado['login'],
-		            'nome'=> $resultado['nome'],
-		            'sobrenome'=>$resultado['sobrenome'],
-		            'codigo_perfil'=>$resultado['codigo_perfil'],
-		            'codigo_status'=>$resultado['codigo_status'],
-		            'logado' => true
-		        ));
-
-		        echo json_encode($resultado);
-		        // echo json_encode('ok');
-
-		} else {
-			$this->session->set_userdata( array('logado' => false) );
-			echo json_encode(array("status"=>"falha"));
-		}
-
-       // var_dump($resultado);
-        // echo 'logado ' .  $this->session->userdata('usuario');
-        //echo json_encode($this->session->userdata());
-        //redirect('/welcome');
-	}
-	public function logout() {
-		$usuario = $this->input->post('usuario');
-		// destroi session
-		$this->session->sess_destroy();
-		echo "saiu";
-		
-	}
+	// public function autenticar() {
+	// 	$usuario = $this->input->post('usuario');
+	// 	$senha = $this->input->post('senha');
+	// 	// autenticar
+	// 	$this->load->model('usuario_model');
+	// 	if ($data['usuario'] = $this->usuario_model->listarPorUsuarioSenha($usuario,$senha)) {
+	// 			$res = array();
+	// 			$resultado = array();
+	// 			foreach($data['usuario'] as $usuario) :
+	// 				$resultado['login'] = $usuario['login'];
+	// 				$resultado['nome'] = $usuario['nome'];
+	// 				$resultado['sobrenome'] = $usuario['sobrenome'];
+	// 				$resultado['codigo_perfil'] = $usuario['codigo_perfil'];
+	// 				$resultado['codigo_status'] = $usuario['codigo_status'];
+	// 				array_push($res, $resultado);
+	// 			endforeach;
+	// 			// loga, cria session
+	// 			$this->session->set_userdata( array(
+	// 	            'login'=> $resultado['login'],
+	// 	            'nome'=> $resultado['nome'],
+	// 	            'sobrenome'=>$resultado['sobrenome'],
+	// 	            'codigo_perfil'=>$resultado['codigo_perfil'],
+	// 	            'codigo_status'=>$resultado['codigo_status'],
+	// 	            'logado' => true
+	// 	        ));
+	// 	        echo json_encode($resultado);
+	// 	        // echo json_encode('ok');
+	// 	} else {
+	// 		$this->session->set_userdata( array('logado' => false) );
+	// 		echo json_encode(array("status"=>"falha"));
+	// 	}
+ //       // var_dump($resultado);
+ //        // echo 'logado ' .  $this->session->userdata('usuario');
+ //        //echo json_encode($this->session->userdata());
+ //        //redirect('/welcome');
+	// }
+	// public function logout() {
+	// 	$usuario = $this->input->post('usuario');
+	// 	// destroi session
+	// 	$this->session->sess_destroy();
+	// 	echo "saiu";
+	
+	// }
 }
