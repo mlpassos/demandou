78a04d6ca79508e0036d5f33d42781cc59bde299
Márcio Passos <marciopassosbel@gmail.com>
2015-12-27 01:54:44 -0300

    New View
    Adding new projects view

---
+++ application/views/admin/projetos/adicionar_sucesso_view.php
@@ -0,0 +1,13 @@
+<div class="container ajaxload">
+<?php if ($this->session->userdata('logado')==true) { ?>
+<div class="row">
+	<!-- <div class="col-lg-4"></div> -->
+	<div class="col-lg-12 col-md-12">
+		<div class="well bg-success">
+			O projeto <strong><?php echo $titulo; ?></strong> foi adicionado com sucesso.
+		</div>
+	</div>
+	<!-- <div class="col-lg-4"></div> -->
+</div>
+<?php } ?>
+</div><!-- /.container -->
